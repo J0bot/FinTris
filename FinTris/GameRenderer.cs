581a2f7920ee4d51320ae725247c3d2f01ba3e37
Ahmad <78198469+ajan0@users.noreply.github.com>
2021-02-12 13:35:47 +0100

    Supression code inutile

--- FinTris/GameRenderer.cs
+++ FinTris/GameRenderer.cs
@@ -54,37 +54,6 @@ namespace FinTris
 
         public void Refresh(SquareState[,] board)
         {
-            
-            #region tests
-            //int startX = 0;
-            //int startY = 0;
-
-            //int endX = 0;
-            //int endY = 0;
-
-            //switch (rot)
-            //{
-            //    case RotationState.Rotation0:
-            //        startX = 0;
-            //        startY = 0;
-            //        break;
-            //    case RotationState.Rotation1:
-            //        startX = 3;
-            //        startY = 0;
-            //        break;
-            //    case RotationState.Rotation2:
-            //        startX = 0;
-            //        startY = 3;
-            //        break;
-            //    case RotationState.Rotation3:
-            //        startX = 3;
-            //        startY = 3;
-            //        break;
-            //    default:
-            //        break;
-            //}
-            #endregion
-
             for (int j = 0; j < _game.Rows; j++)
             {
                 for (int i = 0; i < _game.Cols; i++)
