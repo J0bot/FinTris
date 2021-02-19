07ff507d228488b3e9a2baf7f42cd7e5f9ef00e3
Ahmad <78198469+ajan0@users.noreply.github.com>
2021-02-19 13:42:10 +0100

    Création classe Vector2

---
+++ FinTris/Vector2.cs
@@ -0,0 +1,20 @@
+﻿namespace FinTris
+{
+    public class Vector2
+    {
+        public static Vector2 Zero  =   new Vector2(0, 0);
+        public static Vector2 Up    =   new Vector2(0, 1);
+        public static Vector2 Down  =   new Vector2(0, -1);
+        public static Vector2 Right =   new Vector2(1, 0);
+        public static Vector2 Left  =   new Vector2(-1, 0);
+
+        public readonly int X;
+        public readonly int Y;
+
+        public Vector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
