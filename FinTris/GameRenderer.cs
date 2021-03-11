8f95faeecf2cc6607f47e3235df822be11976b19
Josécarlos Gasser <px01evl@etmlnet.local>
2021-03-11 14:15:43 +0100

    Update de l'entête

--- FinTris/GameRenderer.cs
+++ FinTris/GameRenderer.cs
@@ -1,4 +1,9 @@
-﻿using System.Timers;
+﻿///ETML
+///Auteur   	: José Carlos Gasser, Ahmad Jano, Maxime Andrieux, Maxence Weyermann, Larissa Debarros
+///Date     	: 09.03.2021
+///Description  : Fintris
+
+using System.Timers;
 using System;
 
 namespace FinTris
