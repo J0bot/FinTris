67222819a5e6e9a2f50f76d2b2d73f6a7c795326
ajan0 <78198469+ajan0@users.noreply.github.com>
2021-06-07 22:40:25 +0200

    Test unitaires

---
+++ Fintris.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fintris.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fintris.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dffae80b-9a37-4ee1-abd0-f36ae12ec93a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Fintris.Tests/TetrominoTests.cs
@@ -0,0 +1,30 @@
+﻿using FinTris;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Fintris.Tests
+{
+    [TestClass]
+    public class TetrominoTests
+    {
+        [TestMethod]
+        public void MoveTest()
+        {
+            // Arrange
+            Random rand = new Random();
+            Tetromino tetro = new Tetromino(TetrominoType.Squarie, Vector2.Zero);
+            Vector2 oldPos = tetro.Position;
+            Vector2 expected = new Vector2(rand.Next(10), rand.Next(10));
+            Vector2 actual;
+
+            // Act
+            tetro.Move(expected);
+            actual = tetro.Position;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(oldPos, tetro.PreviousPosition);
+        }
+    }
+}
